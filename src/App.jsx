bac2b63ba63842c1fb9b03249e720c048030f3c9
Thaynar Santana <thaynar.santana@gmail.com>
2023-09-12 23:44:35 -0300

    feat: instala e configura tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Button } from "@nextui-org/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-wrap gap-4 items-center">
+      <Button color="default">
+        Default
+      </Button>
+      <Button color="primary">
+        Primary
+      </Button>
+      <Button color="secondary">
+        Secondary
+      </Button>
+      <Button color="success">
+        Success
+      </Button>
+      <Button color="warning">
+        Warning
+      </Button>
+      <Button color="danger">
+        Danger
+      </Button>
+    </div>
+  );
 }
-
-export default App
