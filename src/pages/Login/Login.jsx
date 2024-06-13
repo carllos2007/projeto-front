47466afcf74855f4e55b757e75b56a6c559b0fdc
Carlos Roberto <carlosroberto.os77@gmail.com>
2024-06-13 20:37:17 -0300

    feat: adiciona imagem feita por ia na tela de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,36 +6,38 @@ import { MdOutlineEmail, MdLockOutline } from "react-icons/md";
 export default function Login() {
 
     return (
-        <main className="w-screen h-screen dark text-foreground bg-background flex flex-col items-center justify-center h-screen">
+        <main className="w-screen h-screen dark text-foreground bg-background items-center justify-center h-screen grid grid-cols-5 grid-rows-5 gap-4" >
+            <img className="col-span-3 row-span-5" src="src\assets\predio.png" />
+            <div className="col-span-2 row-span-5 col-start-4 flex flex-col items-center justify-center h-64">
+                <Input
+                    className="w-full max-w-md p-6"
+                    autoFocus
+                    endContent={
+                        <MdOutlineEmail
+                            className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                    }
+                    label="Email"
+                    placeholder="Digite seu email"
+                    variant="bordered"
+                    isRequired
+                    type="email"
+                />
+                <Input
+                    className="w-full max-w-md p-6"
+                    isRequired
+                    endContent={
+                        <MdLockOutline className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                    }
+                    label="Senha"
+                    placeholder="Digite sua senha"
+                    type="password"
+                    variant="bordered"
+                />
 
-            <Input
-                className="w-full max-w-md p-6"
-                autoFocus
-                endContent={
-                    <MdOutlineEmail
-                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                }
-                label="Email"
-                placeholder="Digite seu email"
-                variant="bordered"
-                isRequired
-                type="email"
-            />
-            <Input
-                className="w-full max-w-md p-6"
-                isRequired
-                endContent={
-                    <MdLockOutline className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                }
-                label="Senha"
-                placeholder="Digite sua senha"
-                type="password"
-                variant="bordered"
-            />
-
-            <Button color="primary" onPress={console.log('logar')}>
-                Entrar
-            </Button>
+                <Button color="primary" onPress={console.log('logar')}>
+                    Entrar
+                </Button>
+            </div>
         </main>
     );
 }
