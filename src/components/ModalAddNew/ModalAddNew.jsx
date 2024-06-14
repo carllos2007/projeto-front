61008f7b1032b7c60d645c24e6c9044f4e9616c9
Carlos Roberto <carlosroberto.os77@gmail.com>
2024-06-13 23:32:42 -0300

    refactor: extrai o modal para um componente separado

---
+++ src/components/ModalAddNew/ModalAddNew.jsx
@@ -0,0 +1,51 @@
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button } from "@nextui-org/react";
+import { Input } from "postcss";
+
+const ModalAddNew = ({ isOpen, closeModal }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={closeModal} placement="top-center">
+      <ModalContent>
+        <ModalHeader className="flex flex-col gap-1">Cadastro de Encomendas</ModalHeader>
+        <ModalBody>
+          <Input
+            name="apartamento"
+            autoFocus
+            label="Apartamento"
+            variant="bordered"
+          />
+          <Input
+            name="nome"
+            autoFocus
+            label="Nome"
+            variant="bordered"
+          />
+          <Input
+            name="remetente"
+            autoFocus
+            label="Remetente"
+            variant="bordered"
+          />
+          <Input
+            name="data"
+            label="Data do Recebimento"
+            placeholder="."
+            type="date"
+            variant="bordered"
+          />
+
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" variant="flat" onPress={closeModal}>
+            Cancelar
+          </Button>
+          <Button color="primary" onPress={closeModal}>
+            Confirmar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+
+  )
+}
+
+export default ModalAddNew
