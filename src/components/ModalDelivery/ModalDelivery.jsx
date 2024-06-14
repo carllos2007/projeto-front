7cd862bc2dca62a174a13cc830d3a85aed7bf00e
Carlos Roberto <carlosroberto.os77@gmail.com>
2024-06-13 23:33:11 -0300

    feat: cria modal de entrega da encomenda

---
+++ src/components/ModalDelivery/ModalDelivery.jsx
@@ -0,0 +1,37 @@
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button } from "@nextui-org/react";
+import { Input } from "postcss";
+
+const ModalDelivery = ({ isOpen, closeModal }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={closeModal} placement="top-center">
+      <ModalContent>
+        <ModalHeader className="flex flex-col gap-1">Entrega de Encomendas</ModalHeader>
+        <ModalBody>
+          <Input
+            name="nome"
+            autoFocus
+            label="Nome"
+            variant="bordered"
+          />
+          <Input
+            name="cpf"
+            autoFocus
+            label="CPF"
+            variant="bordered"
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" variant="flat" onPress={closeModal}>
+            Cancelar
+          </Button>
+          <Button color="primary" onPress={closeModal}>
+            Confirmar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+
+  )
+}
+
+export default ModalDelivery
